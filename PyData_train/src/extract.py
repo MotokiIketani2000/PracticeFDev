3ac81030201b81c88116ff8c1b3bc70981b3dedc
MotokiIketani2000 <music.motto.2000@gmail.com>
2026-05-25 19:54:34 +0900

    新しくectractの修正を行った

--- PyData_train/src/extract.py
+++ PyData_train/src/extract.py
@@ -6,7 +6,7 @@ DEFAULT_DATA_PATH = PROJECT_ROOT / "data" / "raw" / "data.csv"
 
 
 def extract_data(file_path: Path) -> pd.DataFrame:
-    df = pd.read_csv(file_path, encoding="shift_jis")
+    df = pd.read_csv(file_path, encoding="shift_jis",header=2)
     print("データの最初の３行は")
     print(df.head(3))
     return df
